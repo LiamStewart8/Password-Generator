73f66a57b7bdf5fbf0256ebe9c54269de18f53e7
Liam Stewart <liamchristopher.media@gmail.com>
2021-03-26 15:19:44 -0700

    add variables

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,29 @@
-// Assignment Code
+// variables for the random generator
+upperCaseLetters = "abcdefghijklmnopqrstuvwxyz" .split('');
+lowerCaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" .split('');
+numberForPassword = "123456789" .split('');
+symbolForPassword = "!@#$%^&*" .split('');
+maxChar = 8-128;
+allVals = upperCaseLetters + lowerCaseLetters + numberForPassword + symbolForPassword + maxChar;
+
+
+function generatePassword(){
+  var upperCaseLetters = window.confirm('Would you like uppercase letters?');
+  var lowerCaseLetters = window.confirm('Would you like lowercase letters?');
+  var numberForPassword = window.confirm('Would you like numbers?');
+  var symbolForPassword = window.confirm('Would you like symbols?');
+  var maxChar = window.prompt('How many characters would you like your password to be? Minimum:8 Maximum:128');
+
+if (symbolForPassword, lowerCaseLetters, upperCaseLetters, numberForPassword){
+  for ( i=0; i < maxChar + 1; i++){
+    tempChar = Math.floor(Math.random*(maxChar))
+    tempChar = allVals[tempChar]
+    passwordText.append(tempChar);
+  }
+}
+}
+
+ // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
@@ -12,3 +37,4 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
